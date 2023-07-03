e521e898adb22ba0a0c0fa006737a3807865f8ca
marciliodevjava <marciliodevjava@gmail.com>
2023-07-03 15:45:49 -0300

    Curso de python Básico

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#Ola mundo
 print("Olá mundo")
-
+#Brasil ganhou 5 titulos mundias
 print("Brasil", "ganhou", 5, "titulos mundiais", sep=" ")
+
+pais = "Italia"
+
+dia = 1
+mes = 11
+ano = 1994
+
+print('Meu aniversário é')
+
+print(dia, mes, ano, sep="/")
