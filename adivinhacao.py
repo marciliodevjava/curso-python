d9d4f8d4da935b1f5426477d8bcf7af02127a057
marciliodevjava <marciliodevjava@gmail.com>
2023-07-03 17:21:34 -0300

    Curso Básico de python

---
+++ adivinhacao.py
@@ -0,0 +1,25 @@
+bem_vindo = "  Bem vindo ao jogo de advinhação!  "
+fim_execucao = "        Fim da execucao          "
+asterisco = "************************************"
+
+print(asterisco)
+print(bem_vindo)
+print(asterisco)
+
+numero_secreto = 42
+
+
+nome = input("Digite o seu nome:")
+numero = int(input("Digite o seu número: "))
+
+print("Você digitou:", numero, sep=" ")
+if(numero_secreto == numero):
+    print("Tenho uma boa noticia", nome + ".", sep=" ",)
+    print("Você Acertou!!!")
+else:
+    print("Infelizmente você errou", nome + ".", sep=" ")
+    print("Você Errou!!!")
+
+print(asterisco)
+print(fim_execucao)
+print(asterisco)
