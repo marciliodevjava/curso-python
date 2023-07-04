6c3521426595b21d24cc6da7327a100323fbb80c
marciliodevjava <marciliodevjava@gmail.com>
2023-07-04 15:24:23 -0300

    Update adivinhacao

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,27 +1,43 @@
-bem_vindo = "  Bem vindo ao jogo de advinhação!  "
-fim_execucao = "          Fim do Jogo            "
-asterisco = "************************************"
+import random
+
+bem_vindo = "    Bem vindo ao jogo de advinhação!    "
+fim_execucao = "            Fim do Jogo              "
+asterisco = "****************************************"
 
 print(asterisco)
 print(bem_vindo)
 print(asterisco)
 
-numero_secreto = 10
+numero_secreto = random.randint(0, 50)
 loop = True
 
-nome = input("Digite o seu nome:")
+nome = input("Digite o seu nome: ")
+idade = input("Quantos anos você tem " + nome + "?")
 
 while loop:
-    numero = int(input("Digite o seu número entre 0 a 10: "))
+    numero = int(input("Digite o seu número entre 0 a 50: "))
+    acertou = numero_secreto == numero
+    maior = numero > numero_secreto
+    menor = numero < numero_secreto
     print("Você digitou:", numero, sep=" ")
-    if (numero_secreto == numero):
+    if (acertou):
         print("Tenho uma boa noticia", nome + ".", sep=" ", )
         print("Você Acertou!!!")
+        print("Você é muito inteligente e só tem", idade, "anos!", sep=" ")
         loop = False
     else:
-        print("Infelizmente você errou", nome + ".", sep=" ")
-        print("Você Errou!!!")
-        print("Tente novamente.")
+        if maior:
+            print(asterisco)
+            print("Infelizmente você errou", nome + ".", sep=" ")
+            print("É um número inferior a esse numero:", numero, sep=" ")
+            print("Tente novamente.")
+            print(asterisco)
+        elif menor:
+            print(asterisco)
+            print("Infelizmente você errou", nome + ".", sep=" ")
+            print("É um número superior a esse numero:", numero, sep=" ")
+            print("Tente novamente.")
+            print(asterisco)
 
 print(asterisco)
 print(fim_execucao)
