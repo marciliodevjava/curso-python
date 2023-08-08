ba4e8af5d3b9d4e099323d51ef3f2f7a7d06ef07
marciliodevjava <marciliodevjava@gmail.com>
2023-08-07 21:42:47 -0300

    Update nota

--- nota.py
+++ nota.py
@@ -9,15 +9,14 @@ print(bem_vindo)
 print(asterisco)
 
 numero_secreto = random.randint(0, 50)
-loop = True
 
-nome = int(input("Digite a quantidade de aluno"))
 nome = input("Digite o seu nome: ")
-tentativas = 0;
+idade = int(input("Digite a sua idade: "))
+tentativas = int(input("Quantas tentativas? "))
 
-while loop:
+while (tentativas > 0):
+    print("Tentativa nº:", tentativas, sep="")
     numero = int(input("Digite o seu número entre 0 a 50: "))
-    tentativas += 1
     acertou = numero_secreto == numero
     maior = numero > numero_secreto
     menor = numero < numero_secreto
@@ -25,9 +24,8 @@ while loop:
     if (acertou):
         print("Tenho uma boa noticia", nome + ".", sep=" ", )
         print("Você Acertou!!!")
-        print("Tetativas: ", tentativas, sep=" ")
         print("Você é muito inteligente e só tem", idade, "anos!", sep=" ")
-        loop = False
+        tentativas = 0
     else:
         if maior:
             print(asterisco)
@@ -35,12 +33,14 @@ while loop:
             print("É um número inferior a esse numero:", numero, sep=" ")
             print("Tente novamente.")
             print(asterisco)
+            tentativas = tentativas - 1
         elif menor:
             print(asterisco)
             print("Infelizmente você errou", nome + ".", sep=" ")
             print("É um número superior a esse numero:", numero, sep=" ")
             print("Tente novamente.")
             print(asterisco)
+            tentativas = tentativas - 1
 
 
 print(asterisco)
