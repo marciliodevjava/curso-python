2b365799887de4bf2dbbfa7a43cd370efd9a6ff6
marciliodevjava <marciliodevjava@gmail.com>
2023-08-08 08:44:02 -0300

    Update teste

--- teste.py
+++ teste.py
@@ -1,4 +1,26 @@
-for i in range(1,8):
-    if(i == 5):
+for i in range(1, 100 + 1):
+    if (i == 5):
         continue
-    print(i)
+    print("Número: {:03d}".format(i))
+
+# Float
+print("Valores Float")
+print("Valor R$ {:07.2f}".format(126.5))
+print("Valor R$ {:07.2f}".format(51.5))
+print("Valor R$ {:07.2f}".format(845.6))
+print("Valor R$ {:07.2f}".format(151.51))
+print()
+# Inteiro
+print("Valores Inteiros")
+print("Valor R$ {:07d}".format(126))
+print("Valor R$ {:07d}".format(51))
+print("Valor R$ {:07d}".format(8456))
+print("Valor R$ {:07d}".format(15151))
+print()
+# Data
+print("Datas")
+print("Data {:02d}/{:02d}/{:4d}".format(1, 2, 2023))
+print("Data {:02d}/{:02d}/{:4d}".format(1, 5, 2023))
+print("Data {:02d}/{:02d}/{:4d}".format(11, 12, 2023))
+print("Data {:02d}/{:02d}/{:4d}".format(9, 8, 2023))
+print()
