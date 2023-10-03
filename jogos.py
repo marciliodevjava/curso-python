04a98daa45c53292437853a46ea65c65258b4a4b
marciliodevjava <marciliodevjava@gmail.com>
2023-10-03 18:08:27 -0300

    Update jogos.py

--- jogos.py
+++ jogos.py
@@ -1,16 +1,19 @@
 import forca
 import adivinhacao
 
-print("******************************************")
-print("**********  Escolha o seu jogo  **********")
-print("******************************************")
+def escolhe_jogo():
+    print("******************************************")
+    print("**********  Escolha o seu jogo  **********")
+    print("******************************************")
 
-print("(1) Forca (2) Adivinhação")
-jogo = int(input("Digite o número do jogo: "))
+    print("(1) Forca (2) Adivinhação")
+    jogo = int(input("Digite o número do jogo: "))
 
-if (jogo == 1):
-    print("Jogo da Forca Selecionado")
-    forca.jogar()
-elif (jogo == 2):
-    print("Jogo da Adivinhação selecionado")
-    adivinhacao.jogar()
+    if (jogo == 1):
+        print("Jogo da Forca Selecionado")
+        forca.jogar()
+    elif (jogo == 2):
+        print("Jogo da Adivinhação selecionado")
+        adivinhacao.jogar()
+
+escolhe_jogo()
