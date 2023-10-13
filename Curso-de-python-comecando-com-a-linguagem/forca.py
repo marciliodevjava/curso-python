dd46d0308129b4e37fc45b1c1ef853c39095ceb7
marciliodevjava <marciliodevjava@gmail.com>
2023-10-13 20:09:57 -0300

    Update forca

--- Curso-de-python-comecando-com-a-linguagem/forca.py
+++ Curso-de-python-comecando-com-a-linguagem/forca.py
@@ -3,12 +3,14 @@ def jogar():
     print("        Bem vindo ao jogo da Forca        ")
     print("******************************************")
 
+    palavra_secreta = "banana"
+    tentativa = len(palavra_secreta)
+
+    print("A palavra tem {} caractere ".format(len(palavra_secreta)))
+
+    while 
+
     print("Fim do jogo")
-
-    palavra_secreta = "pamonha"
-
-    print("Vamos brincar de FORCA, a palavra tem {} caractere.".format(len(palavra_secreta)))
-
 
 if (__name__ == "__main__"):
     jogar()
