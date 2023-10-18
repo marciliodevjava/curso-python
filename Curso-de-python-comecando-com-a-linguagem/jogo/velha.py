26f959fa0db416cf81d7597eed97e62ed4f4bc6d
marciliodevjava <marciliodevjava@gmail.com>
2023-10-17 22:05:47 -0300

    Create velha

---
+++ Curso-de-python-comecando-com-a-linguagem/jogo/velha.py
@@ -0,0 +1,15 @@
+
+
+def jogar():
+    asterisco = "******************************************"
+    bem_vindo = "        Bem-vindo ao Jogo da Velha        "
+
+    print(asterisco)
+    print(bem_vindo)
+    print(asterisco)
+
+    selecionarXOuO = int(input("Você deseja ser o X ou O"))
+
+
+if (__name__ == "__main__"):
+    jogar()
